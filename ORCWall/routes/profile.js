91bd4a7c4c84d4d0abab9157f2008ee2ba42c227
jerbubias <jerbubias@users.noreply.github.com>
2018-11-14 00:43:58 +0000

    Updated

--- ORCWall/routes/profile.js
+++ ORCWall/routes/profile.js
@@ -6,7 +6,10 @@ let User = require('../models/user');
 
 // Profile Route
 router.get('/', ensureAuthenticated, (req, res) => {
-  res.render('profile');
+  res.render('profile', {
+  	first_name: req.user.first_name,
+  	last_name: req.user.last_name
+  });
 });
 
 // Resources Route

d3e6621cb004183c73b620d91519e1f2ffea83c1
caiosimonbreda <caiosimonbreda@gmail.com>
2018-12-17 13:54:43 +0000

    index tabs

--- ORCWall/public/tabs.js
+++ ORCWall/public/tabs.js
@@ -1,2 +1,17 @@
-var resourceModal = document.getElementById("resourcesmodal");
-var addButton = document.getElementById("addfavorite");
+var redditButton = document.getElementById("reddittab");
+var twitterButton = document.getElementById("twittertab");
+
+var twitterContainer = document.getElementById("tweetscontainer");
+var redditContainer = document.getElementById("redditcontainer");
+
+redditButton.addEventListener('click', function(){
+    twitterContainer.style.display="none";
+    redditContainer.style.display="flex";
+});
+
+twitterButton.addEventListener('click', function(){
+    redditContainer.style.display="none";
+    twitterContainer.style.display="flex";
+});
+
+

ea2d7cc5ec245160138cff6cf0c341a376979e7b
caiosimonbreda <caiosimonbreda@gmail.com>
2018-12-17 13:15:18 +0000

    start tabs js

--- ORCWall/public/tabs.js
+++ ORCWall/public/tabs.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-=======
-
 var resourceModal = document.getElementById("resourcesmodal");
 var addButton = document.getElementById("addfavorite");
->>>>>>> 561d56cc7488267ee466675ec40d04222a5c7de3
